a1f1de14c61f20fcd1b3b66e16e0bfb403622689
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-10-08 07:42:00 +0900

    add getting repository

--- lib/bugspots_ex.ex
+++ lib/bugspots_ex.ex
@@ -1,2 +1,12 @@
 defmodule BugspotsEx do
+
+  alias Geef.Repository
+  alias Geef.Reference
+
+  def repo(path) when is_bitstring(path), do: get_repo Repository.open(path)
+  def repo(path), do: get_repo Repository.open(path)
+  def repo, do: get_repo Repository.open(".")
+
+  defp get_repo({:ok, pid}), do: pid
+  defp get_repo({:error, message}), do: IO.puts message
 end

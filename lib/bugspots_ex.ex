5e47f49be5c27d09caf69fbec582984056d0e21a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-10-09 01:20:08 +0900

    add getting oid

--- lib/bugspots_ex.ex
+++ lib/bugspots_ex.ex
@@ -3,10 +3,26 @@ defmodule BugspotsEx do
   alias Geef.Repository
   alias Geef.Reference
 
-  def repo(path) when is_bitstring(path), do: get_repo Repository.open(path)
-  def repo(path), do: get_repo Repository.open(path)
-  def repo, do: get_repo Repository.open(".")
+  @master "refs/heads/master"
+
+  def repo!(path), do: get_repo Repository.open(path)
+  def repo!, do: repo!(".")
 
   defp get_repo({:ok, pid}), do: pid
-  defp get_repo({:error, message}), do: IO.puts message
+  defp get_repo({:error, message}), do: message
+
+  def references!(path) do
+    case Repository.open(path) do
+      {:ok, pid} -> Repository.reference_names(pid)
+      {:error, message} -> message
+    end
+  end
+  def references!, do: references!(".")
+
+  def oid!(path, branch \\ @master) do
+    case Repository.open(path) do
+      {:ok, pid} -> Geef.Reference.dwim(pid, branch)
+      {:error, message} -> message
+    end
+  end
 end
